cecd681d3f22feff6b8c033d318be2e09fc048a6
mvesign <git@mvesign.com>
2026-01-30 13:14:34 +0100

    Finalize exercise 6

---
+++ UmbracoFundamentals.Models/Constants/ImageCropAliases.cs
@@ -0,0 +1,8 @@
+﻿namespace UmbracoFundamentals.Models.Constants;
+
+public static class ImageCropAliases
+{
+    public const string BlogThumbnail = nameof(BlogThumbnail);
+
+    public const string BlogHeader = nameof(BlogHeader);
+}
