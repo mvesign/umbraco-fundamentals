f97502f9dd88e002d5520393853a3011240c2586
mvesign <git@mvesign.com>
2026-01-30 17:32:54 +0100

    Finalize exercise 10

---
+++ UmbracoFundamentals.Models/Constants/TranslationKeys.cs
@@ -0,0 +1,12 @@
+﻿namespace UmbracoFundamentals.Models.Constants;
+
+public static class TranslationKeys
+{
+    public const string LanguagesPrefix = "Languages - ";
+
+    public const string HomeOurCourses = "Home - Our Courses";
+    
+    public const string HomeOurPartners = "Home - Our Partners";
+    
+    public const string HomeWhatWillItBeToday = "Home - What Will It Be Today";
+}
